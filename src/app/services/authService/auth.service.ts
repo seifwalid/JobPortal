ded4d019a521afda22359b8fe75710b30cbefed0
ahmedsenousy01 <ahmedsenousy01@gmail.com>
2024-04-29 14:30:26 +0300

    added todo list in readme file

--- src/app/services/authService/auth.service.ts
+++ src/app/services/authService/auth.service.ts
@@ -1,71 +1,54 @@
-import { company } from './../../models/companyModel';
-import { User } from './../../models/userModel';
-
 import { Injectable } from '@angular/core';
-import { Auth } from '@angular/fire/auth'; 
-import {AngularFireAuth} from '@angular/fire/compat/auth';
-import { SessionService } from '../sessionService/session.service'; 
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { SessionService } from '../sessionService/session.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
   constructor(
-    private auth :AngularFireAuth ,
-    private sessionService: SessionService) { 
-   }
+    private auth: AngularFireAuth,
+    private sessionService: SessionService) { }
 
-    async registerSeeker (User:any): Promise<void> { 
-      let res =await this.auth.createUserWithEmailAndPassword(User.email, User.password); 
-      if(res.user){
-        this.sessionService.save('uid', res.user.uid); 
-        this.sessionService.save('jobRole', 'seeker');
-        const user = res.user;
-      }
-      window.location.href = '/login';
-
+  async registerSeeker(User: any): Promise<void> {
+    const res = await this.auth.createUserWithEmailAndPassword(User.email, User.password);
+    if (res.user) {
+      this.sessionService.save('uid', res.user.uid);
+      this.sessionService.save('jobRole', 'seeker');
     }
-  
-    async registerRecruiter(company:any): Promise<void> {
-
-        let res =await this.auth.createUserWithEmailAndPassword(company.email, company.password);
-
-        if(res.user){
-          this.sessionService.save('uid', res.user.uid);
-          this.sessionService.save('jobRole', 'company');
-          const user = res.user;
-        }
-
-        window.location.href = '/login';
-
-
-    }
-      async loginCompany(email:string, password:string): Promise<void> {
-        let res = await this.auth.signInWithEmailAndPassword(email, password);
-        if(res.user){
-          this.sessionService.save('uid', res.user.uid);
-          this.sessionService.save('jobRole', 'company');
-          const user = res.user;
-        }
-        window.location.href = '/company';
-
-      }
-
-      async loginSeeker(email:string, password:string): Promise<void> {
-        let res = await this.auth.signInWithEmailAndPassword(email, password);
-        if(res.user){
-          this.sessionService.save('uid', res.user.uid);
-          this.sessionService.save('jobRole', 'seeker');
-          const user = res.user;
-        }
-        window.location.href = '/seeker';
-
-      }
-
-      async logout(): Promise<void> { 
-        await this.auth.signOut();
-        this.sessionService.clear();
-        window.location.href = '/login';
-      }
-  
+    window.location.href = '/login';
   }
+
+  async registerRecruiter(company: any): Promise<void> {
+    const res = await this.auth.createUserWithEmailAndPassword(company.email, company.password);
+
+    if (res.user) {
+      this.sessionService.save('uid', res.user.uid);
+      this.sessionService.save('jobRole', 'company');
+    }
+    window.location.href = '/login';
+  }
+  async loginCompany(email: string, password: string): Promise<void> {
+    const res = await this.auth.signInWithEmailAndPassword(email, password);
+    if (res.user) {
+      this.sessionService.save('uid', res.user.uid);
+      this.sessionService.save('jobRole', 'company');
+    }
+    window.location.href = '/company';
+  }
+
+  async loginSeeker(email: string, password: string): Promise<void> {
+    const res = await this.auth.signInWithEmailAndPassword(email, password);
+    if (res.user) {
+      this.sessionService.save('uid', res.user.uid);
+      this.sessionService.save('jobRole', 'seeker');
+    }
+    window.location.href = '/seeker';
+  }
+
+  async logout(): Promise<void> {
+    await this.auth.signOut();
+    this.sessionService.clear();
+    window.location.href = '/login';
+  }
+}
